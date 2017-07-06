9137e1bafb6d1d2f8c58ad5cd5bde1a35e5dc227
Dovydas Venckus <dovydas.venckus@live.com>
2017-07-06 22:18:41 +0300

    Added simple script for pulling current weather conditions for city

---
+++ pull_weather.groovy
@@ -0,0 +1,21 @@
+import groovy.json.JsonSlurper
+
+String API_KEY = 'YOUR API KEY FROM OPEN WEATHER MAP'
+
+class City{
+  String country
+  String city
+}
+
+def getWeather(String apiKey, City city) {
+  String units = 'metric'
+
+  def apiUrlString = "http://api.openweathermap.org/data/2.5/weather?q=${city.city},${city.country}&units=${units}&appid=${apiKey}"
+
+  def apiUrl = new URL(apiUrlString)
+  return new JsonSlurper().parseText(apiUrl.text)
+}
+
+City vilnius = new City(country: 'LT', city: 'Vilnius')
+
+println getWeather(API_KEY,vilnius)
